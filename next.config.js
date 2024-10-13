edb656810ea3dc5f6d147636ed601f029394e0c3
bits-io <bits1933@gmail.com>
2024-10-13 10:17:06 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export"
+  output: "export",
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'], // Include all the possible extensions
+  webpack(config) {
+    return config;
+  },
+  experimental: {
+    // Enable this if you use any Next.js experimental features
+  },
 }
 
 module.exports = nextConfig
